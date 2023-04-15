ed1cd9ad459ae36bb5f48d56f424dc82143a40ea
abdul <abdulahisholabi@gmail.com>
2023-04-15 15:20:05 -0700

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+       backgroundImage : {
+              front : "url('../public/img/pexels-francesco-altamura-10043475.jpg')"
+       }
+    },
+  },
+  plugins: [],
+}
+
