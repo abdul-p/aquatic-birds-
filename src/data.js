dcb869c3c07c919c97e520488f65e2c660169c4b
abdul <abdulahisholabi@gmail.com>
2023-04-23 13:33:51 -0700

    Added the content function

--- src/data.js
+++ src/data.js
@@ -2,29 +2,36 @@
 let Data ;
 
 export default Data = [
-    {
-        Diseases : {
-            diseaseP : "Outbreak of diseases spread by waterbirds result from the transition of water-borne viruses to those wild birds. the spread can be caused by dead waterbirds in the vicinity of other organism, or simply from waterirds with the virus settling into more densely populated areas (whether by humans or other organism)." ,
-            types : {
-                 DEV : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 H5N1 : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 Histoplasmosis : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 Mycobacterium : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 Cryptococcus : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 Erysipelas : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 Ornthosis : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 WestNile : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-                 Lyme : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." ,
-            }
+    
+       {
+            'id' : 'di' ,
+            'name' : " Diseases" ,
+            'def' : "Outbreak of diseases spread by waterbirds result from the transition of water-borne viruses to those wild birds. the spread can be caused by dead waterbirds in the vicinity of other organism, or simply from waterirds with the virus settling into more densely populated areas (whether by humans or other organism)." ,
+            'types' : [
+                 { 'name' : 'DEV' , 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." },
+                 { 'name' : 'H5N1', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." },
+                 { 'name' : 'Histoplasmosis', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds."} ,
+                 { 'name' : 'Mycobacterium', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds."} ,
+                 { 'name' : 'Cryptococcus', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds."} ,
+                 { 'name' : 'Erysipelas', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." },
+                 { 'name' : 'Ornthosis', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." },
+                 { 'name' : 'WestNile', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds."} ,
+                 { 'name' : 'Lyme', 'text' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." },
+                ]
            
-        } ,
-
-        Extinction : {
-            Text : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds. "
         },
 
-        InterspecificCompetition : {
-            Text : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." 
+        {
+            'id' : 'ex' ,
+            'name' : 'Extinction' ,
+            'def' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds. "
+        },
+
+        {
+            'id' : 'in' ,
+           'name' : 'Interspecific Competition',
+            'def' : "Duck plague (DP), also called duck entries virus (DEV), presents the most important concern in mass waterfowl production. Free-ranging water birds are most likely infectious carriers. while the overall epidemology of DEV is unknown in western Europe , studies conducted in poland agree with the high levels of transmission between free-ranging water birds." 
         }
-    }
+
+    
 ]
--- src/script.js
+++ src/script.js
@@ -3,13 +3,50 @@ import Data from './data.js';
 
 // const [{ Diseases , Extinction , InterspecificCompetition }] = Data ;
 
-// console.log(Diseases);
+// function displayTypes (data) {
+//     let typ , ul , li  , h2 , p;
+//            typ = data.types ;
+//            ul = document.createElement('ul');
+           
+//      typ.forEach(cur => {
+//            li = document.createElement('li');
+//            ul.appendChild(li);
+//            h2 = document.createElement('h2');
+//            h2.innerText = cur.name ;
+//            p = document.createElement('');
+//            p.innerText = cur.text ;
+//            li.appendChild(h2);
+//            li.appendChild(p);
+//            document.querySelector('.data-p').insertAdjacentHTML('afterend', ul);
+//            console.warn(ul)
+//      })
+// }
 
 
+function displayData (data , dom , head , type) {
+     
+     data.forEach( cur => {
+          if(type === cur.id) {
+               dom.innerText = '' ;
+               console.log(cur.id);
+                    head.innerText = cur.name ;
+                    let p = document.createElement('p');
+                    p.innerText = cur.def ;
+                    p.className = 'data-p'
+                    dom.insertAdjacentElement('afterbegin' , p);
+                    if (cur.type) {
+                         // displayTypes(cur)
+                    }
+               }
+               
+          });
 
+}
 
 const page = (function() {
-     let list ;
+     let list , dom , head ;
+          dom = document.getElementById('data-dis');
+          head = document.getElementById('data-head');
      // toggle the content element  
      document.getElementById('content').addEventListener('click' , () => {
          let lists = document.getElementById('lists');
@@ -22,11 +59,13 @@ const page = (function() {
      });
 
      // get and display content element data 
-     list = document.querySelectorAll('.list');
-     
-     list.addEventListener('click' , cur => {
-          console.log(cur.id);
+     list = document.getElementById('lists');
+     list.addEventListener('click' , (e) => {
+               let type = e.target.id ;
+               console.log(type);
+               displayData(Data,dom,head,type);
      });
 
 
 })();
+
